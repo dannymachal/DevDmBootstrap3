c64e6cbd662a6dfca9f6cceca18aa707e5986184
Andrew Minion <andrew@andrewrminion.com>
2017-01-24 16:31:09 -0500

    use child theme’s bootstrap.css (if applicable)

--- devdmbootstrap3/functions.php
+++ devdmbootstrap3/functions.php
@@ -27,7 +27,7 @@
 ////////////////////////////////////////////////////////////////////
     function devdmbootstrap3_theme_stylesheets()
     {
-        wp_enqueue_style('bootstrap.css', get_template_directory_uri() . '/css/bootstrap.css', array(), '1', 'all' );
+        wp_enqueue_style('bootstrap.css', get_stylesheet_directory_uri() . '/css/bootstrap.css', array(), '1', 'all' );
         wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), array(), '1', 'all' );
     }
     add_action('wp_enqueue_scripts', 'devdmbootstrap3_theme_stylesheets');
